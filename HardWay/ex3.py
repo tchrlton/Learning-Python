d6dcf315584e5aa16ffcd5d3efbde27584e9e295
Thomas Charlton <tchrlton1@gmail.com>
2018-03-31 19:31:04 -0700

    Exercise 3

---
+++ HardWay/ex3.py
@@ -0,0 +1,35 @@
+#telling the program to say "I will now count my chickens:"
+print ("I will now count my chickens:")
+
+#calculates the number of hens
+print ("Hens", 25 + 30 / 6)
+#calculate the number of roosters
+print ("Roosters", 100 - 25 * 3 % 4)
+
+#telling the program to say "Now I will count the eggs"
+print ("Now I will count the eggs:")
+
+#calculating the following math
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#telling the progrom to say "Is it true that 3 + 2 < 5 -7?"
+print ("Is it true that 3 + 2 < 5 - 7?")
+
+#telling the progrm to say "3 + 2 < 5 - 7?"
+print ("3 + 2 < 5 - 7")
+
+#calculates 3+2
+print ("What is 3 + 2?", 3 + 2)
+#calculates 5-7
+print ("What is 5 - 7?", 5 - 7)
+
+#shows that the above equation is false
+print ("Oh, that's why it's False.")
+
+#telling the program to say "How about some more"
+print ("How about some more.")
+
+#calculates and shows if these are true or false
+print ("Is it greater?", 5 > -2)
+print ("Is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= -2)
