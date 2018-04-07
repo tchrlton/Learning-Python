e286899a75e1c5ac8fcd0d8155c0d06d7fe36f52
Thomas Charlton <tchrlton1@gmail.com>
2018-04-06 22:02:03 -0700

    Exercise 7

---
+++ HardWay/ex7.py
@@ -0,0 +1,28 @@
+#prints out whats in the ""
+print("Mary had a little lamb.")
+#prints out the string and also pulls from the format of just whats in the string
+print("Its fleece was white as %s." % 'snow')
+#prints on the string shown
+print("And everywhere that Mary went.")
+#prints out . but multiples is by 10 so it shows 10 dots.
+print((".") * 10) # what'd that do?
+
+#Each of these will be print out in the order that the value is being shown in the bottom of the code
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+#removing the comma does nothing to the code
+#adding the values together just adds them to the string like a
+print(end1 + end2 + end3 + end4 + end5 + end6,)
+print(end7 + end8 + end9 + end10 + end11 + end12)
