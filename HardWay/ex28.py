8ac1b96669a39d3dcd5a6fd5f822aa269b9126b1
Thomas Charlton <tchrlton1@gmail.com>
2018-04-24 01:24:34 -0700

    Exercise 28

---
+++ HardWay/ex28.py
@@ -0,0 +1,13 @@
+#A way to show how boolean logic works with python
+'''
+I got really good with this so I made my own to test it in python
+'''
+
+#True
+2 == 2
+#True
+4 != 5
+#False
+not("right" == "wrong" or "5 != 7")
+#True
+not(5 >= 4 and 6 != 6)
