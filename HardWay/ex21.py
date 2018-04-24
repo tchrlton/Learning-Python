5dcaedc514f4d6698272dbd1285399329f64d05a
Thomas Charlton <tchrlton1@gmail.com>
2018-04-23 19:38:11 -0700

    Exercise 21

---
+++ HardWay/ex21.py
@@ -0,0 +1,44 @@
+def add(a, b):
+   print("ADDING %d + %d" % (a, b))
+   return a + b
+
+def subtract(a, b):
+   print("SUBTRACTING %d - %d" % (a, b))
+   return a - b
+
+def multiply(a, b):
+   print('MULTIPLYING %d * %d' % (a, b))
+   return a * b
+
+def divide(a, b):
+   print("DIVIDING %d / %d" % (a, b))
+   return a / b
+
+
+print("Let's do some math with just function!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print("Age: %d, Height: % d, Weight: %d, IQ: %d" % (age, height, weight, iq))
+
+
+# A puzzle for the extra credit, type it in anyway
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
+
+user_weight = float(input())
+
+calories = 2100
+protein = multiply(4, multiply(user_weight, 1.0))
+fat = multiply(0.30, calories)
+carbs = subtract(calories, add(protein, fat))
+
+print("This is", divide(protein, 4), "grams in protein.")
+print("This is", divide(fat, 4), "grams in fat.")
+print("This is", divide(carbs, 4), "grams in carbs.")
