02089032f68ed43ffe9809819abd0700f9d00515
Thomas Charlton <tchrlton1@gmail.com>
2018-04-28 16:54:07 -0700

    Exercise 34

---
+++ HardWay/ex34.py
@@ -0,0 +1,31 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#The animal at 1 is a python.
+print(animals[1])
+#The thid (3rd) animal is at 2 and is a peacock.
+print(animals[2])
+#The first (1st) animal is at 0 and is a bear.
+print(animals[0])
+#The animal at 3 is a kangaroo.
+print(animals[3])
+#The fifth (5th) animal is at 4 and is a whale.
+print(animals[4])
+#The animal at 2 is a peacock.
+print(animals[2])
+#The sixth (6th) animal is at 5 and is a platypus.
+print(animals[5])
+#The animal at 4 is a whale.
+print(animals[4])
+
+#The year 2010 in "January 1, 2010" really is 2010 and not 2009 because of
+#how the string itself is written. January is first, then 1 is second,
+#and 2010 is third so it really is 2010.
+
+muscles = ['legs', 'biceps', 'triceps', 'chest', 'shoulders', 'back']
+
+#The muscle at 3 is chest.
+print(muscles[3])
+#The third (3rd) muscle is at 2 and is triceps.\
+print(muscles[2])
+#The muscle at 5 is back.
+print(muscles[5])
